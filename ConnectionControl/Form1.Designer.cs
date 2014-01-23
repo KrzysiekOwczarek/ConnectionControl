e97c918ef5f6d4dfefbbfbc51e86077f927bbd6e
KrzysiekOwczarek <krzysiek.owczarek@onet.eu>
2014-01-23 01:19:14 +0100

    pierwsze kradzierze kodu wykonane

--- ConnectionControl/Form1.Designer.cs
+++ ConnectionControl/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ConnectionControl
 {
-    partial class Form1
+    partial class ConnectionControl
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cloudIPTextBox = new System.Windows.Forms.TextBox();
+            this.cloudPortTextBox = new System.Windows.Forms.TextBox();
+            this.conToCloudButton = new System.Windows.Forms.Button();
+            this.selectedClientBox = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.log = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP chmury sterowania";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(155, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port chmury sterowania";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Klienci";
+            // 
+            // cloudIPTextBox
+            // 
+            this.cloudIPTextBox.Location = new System.Drawing.Point(11, 25);
+            this.cloudIPTextBox.Name = "cloudIPTextBox";
+            this.cloudIPTextBox.Size = new System.Drawing.Size(128, 20);
+            this.cloudIPTextBox.TabIndex = 3;
+            // 
+            // cloudPortTextBox
+            // 
+            this.cloudPortTextBox.Location = new System.Drawing.Point(158, 25);
+            this.cloudPortTextBox.Name = "cloudPortTextBox";
+            this.cloudPortTextBox.Size = new System.Drawing.Size(134, 20);
+            this.cloudPortTextBox.TabIndex = 4;
+            // 
+            // conToCloudButton
+            // 
+            this.conToCloudButton.Location = new System.Drawing.Point(316, 22);
+            this.conToCloudButton.Name = "conToCloudButton";
+            this.conToCloudButton.Size = new System.Drawing.Size(75, 23);
+            this.conToCloudButton.TabIndex = 5;
+            this.conToCloudButton.Text = "POŁĄCZ";
+            this.conToCloudButton.UseVisualStyleBackColor = true;
+            // 
+            // selectedClientBox
+            // 
+            this.selectedClientBox.FormattingEnabled = true;
+            this.selectedClientBox.Location = new System.Drawing.Point(15, 120);
+            this.selectedClientBox.Name = "selectedClientBox";
+            this.selectedClientBox.Size = new System.Drawing.Size(124, 21);
+            this.selectedClientBox.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(155, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Węzły";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(158, 120);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(134, 21);
+            this.comboBox2.TabIndex = 8;
+            // 
+            // log
+            // 
+            this.log.Location = new System.Drawing.Point(15, 217);
+            this.log.Name = "log";
+            this.log.Size = new System.Drawing.Size(376, 96);
+            this.log.TabIndex = 9;
+            this.log.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 201);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Log";
+            // 
+            // ConnectionControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 325);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.log);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.selectedClientBox);
+            this.Controls.Add(this.conToCloudButton);
+            this.Controls.Add(this.cloudPortTextBox);
+            this.Controls.Add(this.cloudIPTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ConnectionControl";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox cloudIPTextBox;
+        private System.Windows.Forms.TextBox cloudPortTextBox;
+        private System.Windows.Forms.Button conToCloudButton;
+        private System.Windows.Forms.ComboBox selectedClientBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.RichTextBox log;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- ConnectionControl/Program.cs
+++ ConnectionControl/Program.cs
@@ -16,7 +16,7 @@ namespace ConnectionControl
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ConnectionControl());
         }
     }
 }
